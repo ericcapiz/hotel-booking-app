7bd0f83eb160ffe399ff4d37974377decf5ed00e
eric capiz <ericcapiz@gmail.com>
2024-04-11 18:30:52 -0500

    added * get req on be

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,6 +24,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
 });
