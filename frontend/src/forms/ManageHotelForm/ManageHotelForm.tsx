96e0ed140f50a0775db0651806b14a8c4824b01f
eric capiz <ericcapiz@gmail.com>
2024-04-13 16:05:51 -0500

    hotel type started

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,5 +1,6 @@
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
 
 export type HotelFormData = {
   name: string;
@@ -21,8 +22,9 @@ const ManageHotelForm = () => {
 
   return (
     <FormProvider {...formMethods}>
-      <form>
+      <form className="flex flex-col gap-10">
         <DetailsSection />
+        <TypeSection />
       </form>
     </FormProvider>
   );
